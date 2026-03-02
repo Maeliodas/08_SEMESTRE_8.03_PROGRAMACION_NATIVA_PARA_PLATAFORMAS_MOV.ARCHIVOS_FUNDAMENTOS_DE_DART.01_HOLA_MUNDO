2b8cf64ea6fee02fb7443ce1b6214d9f6e95b6fd
VICTOR MANUEL ITURBIDE MARIANO <iturbide.mariano.victor.manuel@gmail.com>
2026-03-02 00:28:19 -0600

    PRIMER ARCHIVO DE HOLA MUNDO EN DART

---
+++ (01)_HOLA_MUNDO.dart
@@ -0,0 +1,7 @@
+void main() {
+  //var myname = 'Victor';
+    //String myname = 'Victor';
+  late final myname; //Declarar constantes que no cambian de valor desde su definición, mientras que en const puede cambiar su valor antes de la contrucción
+  myname = 'Victor';
+  print('Hola, ${myname.toUpperCase()}');
+}
